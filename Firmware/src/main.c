c87bc9d740d04790616e7258a2c60cfaad45ceba
unknown <pavel.palonen@gmail.com>
2024-07-04 07:35:59 +0300

    Datouboss silent cooler firmware final

--- Firmware/src/main.c
+++ Firmware/src/main.c
@@ -6,6 +6,60 @@
 #include "main.h"
 #include "InitDevice.h"
 
+typedef struct t_struct {
+   int8_t temp;
+   uint8_t ppwm0;
+   uint8_t ppwm1;
+} TEMP_PWM;
+
+// Temperature -- PWM table
+/*
+TEMP_PWM code temp_pwm_table[22] = {
+    {30, 5},
+    {31, 6},
+    {32, 7},
+    {33, 8},
+    {34, 9},
+    {35, 10},
+    {36, 12},
+    {37, 14},
+    {38, 16},
+    {39, 18},
+    {40, 22},
+    {42, 32},
+    {44, 42},
+    {46, 52},
+    {48, 62},
+    {50, 72},
+    {51, 82},
+    {52, 92},
+    {53, 94},
+    {54, 96},
+    {55, 98},
+    {125, 100},
+ };
+*/
+
+TEMP_PWM code temp_pwm_table[17] = {
+{34, 5, 4},
+{35, 8, 4},
+{36, 10, 4},
+{37, 12, 4},
+{39, 14, 4},
+{40, 16, 5},
+{42, 20, 6},
+{44, 25, 7},
+{46, 30, 9},
+{48, 35, 15},
+{50, 45, 25},
+{51, 55, 36},
+{52, 65, 48},
+{53, 80, 62},
+{54, 90, 74},
+{55, 95, 86},
+{125, 100, 100},
+};
+
 int8_t getTemperature(void) {
     uint8_t i;
     uint16_t adcraw = 0; // raw data from ADC
@@ -24,45 +78,16 @@ int8_t getTemperature(void) {
     return tmp;
 }
 
-void setPWM(uint8_t ppwm){
+void setPWM(uint8_t i){
   // convert percent (0-100) to real pwm (255 - 0). 0 means full speed (100%)
-  uint8_t rawpwm = 255 - ((uint16_t)ppwm * 255 / 100);
+  uint8_t rawpwm0 = 255 - ((uint16_t)temp_pwm_table[i].ppwm0 * 255 / 100);
+  uint8_t rawpwm1 = 255 - ((uint16_t)temp_pwm_table[i].ppwm1 * 255 / 100);
   // set new PWM value for the fan.
-  PCA0CPH0 = rawpwm;
-  PCA0CPH1 = rawpwm;
+  PCA0CPH0 = rawpwm0;
+  PCA0CPH1 = rawpwm1;
 }
 
 
-typedef struct t_struct {
-   int8_t temp;
-   uint8_t ppwm;
-} TEMP_PWM;
-
-// Temperature -- PWM table
-TEMP_PWM code temp_pwm_table[22] = {
-    {30, 30},
-    {31, 31},
-    {32, 32},
-    {33, 33},
-    {34, 34},
-    {35, 35},
-    {36, 36},
-    {37, 37},
-    {38, 38},
-    {39, 39},
-    {40, 40},
-    {42, 43},
-    {44, 48},
-    {46, 58},
-    {48, 70},
-    {50, 80},
-    {51, 82},
-    {52, 84},
-    {53, 86},
-    {54, 88},
-    {55, 90},
-    {125, 100},
- };
 
  void main_routine(void){
     int8_t temperature;
@@ -73,9 +98,9 @@ TEMP_PWM code temp_pwm_table[22] = {
 
     temperature = getTemperature();
     // set pwm according to temperature
-    for(i=0;i<22;i++){
+    for(i=0;i<17;i++){
         if(temperature <= temp_pwm_table[i].temp) {
-            setPWM(temp_pwm_table[i].ppwm);
+            setPWM(i);
             break;
         }
     }
@@ -90,9 +115,12 @@ int main(void) {
   // Call hardware initialization routine
   enter_DefaultMode_from_RESET();
   
+  setPWM(0); // set minimum speed
+
   getTemperature(); // initialize ADC
 
   while(1){
+
       main_routine();
 	}
 
